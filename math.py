17c8509522b8726aa8f85000418e35e08b3a3734
Deepak <patildeepak049@gmail.com>
2021-05-28 14:45:35 +0530

    Added indentation of the code

--- math.py
+++ math.py
@@ -1,1 +1,13 @@
-def add(x,y): pass def subtract(x,y): pass def multiply(x,y): pass def divide(x,y): pass
+def add(x,y): 
+	pass 
+
+def subtract(x,y): 
+	pass 
+
+
+def multiply(x,y): 
+	pass 
+
+
+def divide(x,y): 
+	pass
